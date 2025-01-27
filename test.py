21fc768e983340d37b5ef83dd743e254b10d9c05
Patrick Laptop (MBP) <patirck.heenan@colorado.edu>
2025-01-27 08:41:41 -0500

    added slicing the data

--- test.py
+++ test.py
@@ -29,14 +29,15 @@ class MyTestCase(unittest.TestCase):
         :return: Nothing, sets up the class
         """
         # read in the video
-        cls.video_matrix = video.read_video("data/mario-trim.mov",
-                                            num_frames=10)
+        frames_test_rate = 200
+        cls.video_matrix = video.read_video("data/mario-trim.mov")
         # https://en.wikipedia.org/wiki/Microplate#Formats_and_Standardization_efforts
         cls.resized_videos = {}
         for plate,plate_dict in utilities.plate_to_well_dict().items():
             n_rows,n_cols = plate_dict["n_rows"],plate_dict["n_cols"]
             cls.resized_videos[plate] = \
-                video.resize_video(cls.video_matrix,px_width=n_cols,px_height=n_rows,
+                video.resize_video(cls.video_matrix[::frames_test_rate],
+                                   px_width=n_cols,px_height=n_rows,
                                    disable_tqdm=True)
 
     def _check_plate_read_expected(self,file_name,sheet_to_matrices):
@@ -321,6 +322,7 @@ class MyTestCase(unittest.TestCase):
                 with self.subTest(i=self.i_sub_test,msg=os.path.basename(file_v)):
                     assert all(s_expected == s)
                 self.i_sub_test += 1
+
 
 def save_multiple_plates_to_same_file(plate_df_colors,file_name):
     """
